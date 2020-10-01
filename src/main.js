1f82984b999682d443721c1c66db2e5ec0d16810
zhenshiluosuo <menglongmoon@hotmail.com>
2020-10-01 19:59:07 +0800

    更新依赖

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import axios from 'axios';
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+Vue.use(Vuex);
+Vue.use(VueRouter);
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+
+}).$mount('#app');
